164c8da84741118df1d79af95dfb222a018d5714
Grain Stark <palagend@gmail.com>
2019-09-01 06:25:27 +0000

    gotests

--- init-vim-go.sh
+++ init-vim-go.sh
@@ -5,6 +5,6 @@ set -e
 export GO111MODULE=on
 export GOPROXY=https://goproxy.io
 
-go_pkg_list="github.com/klauspost/asmfmt/cmd/asmfmt github.com/go-delve/delve/cmd/dlv github.com/kisielk/errcheck github.com/davidrjenni/reftools/cmd/fillstruct github.com/mdempsky/gocode github.com/stamblerre/gocode github.com/rogpeppe/godef github.com/zmb3/gogetdoc golang.org/x/tools/cmd/goimports golang.org/x/lint/golint golang.org/x/tools/gopls@latest github.com/alecthomas/gometalinter github.com/golangci/golangci-lint/cmd/golangci-lint github.com/fatih/gomodifytags golang.org/x/tools/cmd/gorename github.com/jstemmer/gotags golang.org/x/tools/cmd/guru github.com/josharian/impl honnef.co/go/tools/cmd/keyify github.com/fatih/motion github.com/koron/iferr"
+go_pkg_list="github.com/klauspost/asmfmt/cmd/asmfmt github.com/go-delve/delve/cmd/dlv github.com/kisielk/errcheck github.com/davidrjenni/reftools/cmd/fillstruct github.com/mdempsky/gocode github.com/stamblerre/gocode github.com/rogpeppe/godef github.com/zmb3/gogetdoc golang.org/x/tools/cmd/goimports golang.org/x/lint/golint golang.org/x/tools/gopls@latest github.com/alecthomas/gometalinter github.com/golangci/golangci-lint/cmd/golangci-lint github.com/fatih/gomodifytags golang.org/x/tools/cmd/gorename github.com/jstemmer/gotags golang.org/x/tools/cmd/guru github.com/josharian/impl honnef.co/go/tools/cmd/keyify github.com/fatih/motion github.com/koron/iferr github.com/cweill/gotests"
 
 cd $GOPATH && go get -v $go_pkg_list
